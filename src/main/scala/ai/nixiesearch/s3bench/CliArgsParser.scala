51e642540424f3593b9b1c27b683abeb2badb3ff
Grebennikov Roman <grv@dfdx.me>
2024-09-15 12:52:10 +0200

    make cli parser not hang on IOApp

--- src/main/scala/ai/nixiesearch/s3bench/CliArgsParser.scala
+++ src/main/scala/ai/nixiesearch/s3bench/CliArgsParser.scala
@@ -8,20 +8,52 @@ import org.rogach.scallop.{ScallopConf, ScallopOption, Subcommand, throwError, g
 import scala.util.{Try, Success, Failure}
 
 case class CliArgsParser(arguments: List[String]) extends ScallopConf(arguments) with Logging {
-  val bucket = opt[String](name = "bucket", descr = "S3 bucket", required = true)
-  val prefix = opt[String](name = "prefix", descr = "S3 prefix", required = true)
+  val bucket = opt[String](name = "bucket", descr = "S3 bucket (required)", required = true)
+  val prefix = opt[String](name = "prefix", descr = "S3 prefix (required)", required = true)
   val requests =
-    opt[Int](name = "requests", descr = "how many requests to perform", required = false, default = Some(100))
+    opt[Int](
+      name = "requests",
+      descr = "how many requests to perform (optional, default 100)",
+      required = false,
+      default = Some(100)
+    )
 
-  val endpoint = opt[String](name = "endpoint", descr = "S3 endpoint URL", required = false)
+  val endpoint = opt[String](name = "endpoint", descr = "S3 endpoint URL (optional)", required = false)
   val threads =
-    opt[Int](name = "threads", descr = "How many parallel clients to spawn", required = false, default = Some(1))
-  val region = opt[String](name = "region", descr = "AWS region", required = false, default = Some("us-east-1"))
+    opt[Int](
+      name = "threads",
+      descr = "How many parallel clients to spawn (optional, default 1)",
+      required = false,
+      default = Some(1)
+    )
+  val region = opt[String](
+    name = "region",
+    descr = "AWS region (optional, default us-east-1)",
+    required = false,
+    default = Some("us-east-1")
+  )
 
   banner("""Usage: s3bench  <options>
            |Options:
            |""".stripMargin)
   footer("\nFor all other tricks, consult the docs on https://github.com/nixiesearch/s3bench")
+
+  override protected def onError(e: Throwable): Unit = e match {
+    case r: ScallopResult if !throwError.value =>
+      r match {
+        case Help("") =>
+          logger.info("\n" + builder.getFullHelpString())
+        case Help(subname) =>
+          logger.info("\n" + builder.findSubbuilder(subname).get.getFullHelpString())
+        case ScallopVersion =>
+          "\n" + getVersionString().foreach(logger.info)
+        case e @ ScallopException(message) => throw e
+        // following should never match, but just in case
+        case other: ScallopException => throw other
+      }
+    case e => throw e
+  }
+
 }
 
 object CliArgsParser {
